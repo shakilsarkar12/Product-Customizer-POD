8a51ed0bf58a5537cf1172b35e91e38bd79d16b4
Shakil <shakil.sarkar1932@gmail.com>
2026-08-03 17:11:06 +0600

    Configure assetPrefix in next.config.mjs to resolve Shopify App Proxy JS chunk loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.NEXT_PUBLIC_APP_URL || "https://podcraft.shakildev.online",
   /* config options here */
   webpack(config) {
     config.module.rules.push({
